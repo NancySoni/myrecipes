b0c8e16caf36bc4b529f2bbd48ab23e690937cdc
nancysoni <nancysoni789@gmail.com>
2019-04-19 21:27:15 +0530

    complete home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//=bootstrap-sprockets
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
