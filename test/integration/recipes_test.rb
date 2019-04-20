b61e03e73b92fed64a22131e3e2dd4762ea2f5d8
nancysoni <nancysoni789@gmail.com>
2019-04-20 20:09:32 +0530

    complete index and show action in recipe

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -8,7 +8,7 @@ def setup
   @recipe2.save
 
 end
-test"should get recipe index"do
+test"should get recipe index" do
 get recipes_path
 assert_response:success
 
@@ -17,12 +17,23 @@ end
 test "should get recipe listing" do
 get recipes_path
 assert_template 'recipes/index'
-assert_match @recipe.name,response.body
-assert_match @recipe2.name,response.body
+
+assert_select "a[href=?]",recipe_path(@recipe),text:@recipe.name
+
+
+assert_select "a[href=?]",recipe_path(@recipe2),text:@recipe2.name
 end
 
 
+test "should get recipes show" do
+  get recipe_path(@recipe)
+  assert_template 'recipes/show'
 
+  assert_match @recipe.name,response.body
+  assert_match @recipe.description,response.body
+  assert_match @chef.chefname,response.body
+
+end
 
   # test "the truth" do
   #   assert true
