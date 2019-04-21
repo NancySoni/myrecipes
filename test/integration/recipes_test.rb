9639a0c5a6c1aed41e85f29fc45dcbe5436e9216
nancysoni <nancysoni789@gmail.com>
2019-04-21 20:39:41 +0530

    implement edit and delete

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class RecipesTest < ActionDispatch::IntegrationTest
 def setup
   @chef=Chef.create!(chefname:"nancy",email:"nancy@example.com")
@@ -32,7 +31,8 @@ test "should get recipes show" do
   assert_match @recipe.name,response.body
   assert_match @recipe.description,response.body
   assert_match @chef.chefname,response.body
-
+  assert_select 'a[href=?]',edit_recipe_path(@recipe),text:"Edit this recipe"
+  assert_select 'a[href=?]',recipe_path(@recipe),text:"Delete this recipe"
 end
 test "create new valid recipe" do
   get new_recipe_path
@@ -43,12 +43,15 @@ test "create new valid recipe" do
   assert_difference 'Recipe.count',1 do
     post recipes_path,params:{recipe:{name:name_of_recipe,description:description_of_recipe}}
 
-end
+      end
   follow_redirect!
   assert_match name_of_recipe.capitalize,response.body
   assert_match description_of_recipe,response.body
-end
-test "reject invalid recipe" do
+        end
+
+
+
+  test "reject invalid recipe" do
   get new_recipe_path
   assert_template 'recipes/new'
   assert_no_difference 'Recipe.count' do
