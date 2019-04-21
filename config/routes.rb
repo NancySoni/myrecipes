055d5ed3eaa985dce081a9a8a434d870b20d0389
nancysoni <nancysoni789@gmail.com>
2019-04-21 15:13:19 +0530

    add recipe creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
 root "pages#home"
 get 'pages/home', to:'pages#home'
-get '/recipes', to: 'recipes#index'
-get '/recipes/:id',to:'recipes#show', as:'recipe'
+resources:recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
